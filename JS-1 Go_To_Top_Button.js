75da99125a3d2464a05b611fb3c3157fffeccf78
Vishal Singh Rajawat <vsrofficial001@gmail.com>
2024-07-31 03:59:12 -0700

    Initial Commit

---
+++ JS-1 Go_To_Top_Button.js
@@ -0,0 +1,1 @@
+function scrollFunction(){var o=document.getElementById("back-to-top");document.body.scrollTop>800||document.documentElement.scrollTop>800?o.style.display="flex":o.style.display="none"}function scrollToTop(){window.scrollTo({top:0,behavior:"smooth"})}window.onscroll=function(){scrollFunction()};
